4f63b026af150b68c94cdde15095746120deb744
timjmiller <timothy.j.miller@noaa.gov>
2022-03-08 16:25:36 -0500

    modify linreg.R

--- wham_simulation/tutorial/linreg.R
+++ wham_simulation/tutorial/linreg.R
@@ -4,7 +4,7 @@ dyn.load(dynlib("linreg"))
 set.seed(123)
 data <- list(Y=rnorm(10)+1:10,x=1:10)
 parameters <- list(a=0,b=0,logSigma=0)
-obj <- MakeADFun(data,parameters)
+obj <- MakeADFun(data,parameters, DLL="linreg")
 obj$hessian <- TRUE
 opt <- do.call("optim",obj)
 opt
