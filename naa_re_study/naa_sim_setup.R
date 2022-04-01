8441540b984fb4d7ef758606f4b68e43af4c9a3f
Timothy.J.Miller <timothy.j.miller>
2022-04-01 12:45:10 -0400

    saving some work on code for NAA_re_study

---
+++ naa_re_study/naa_sim_setup.R
@@ -0,0 +1,211 @@
+# devtools::load_all("~/work/wham/wham")
+# devtools::install_github("timjmiller/wham", dependencies=TRUE, ref="devel")
+library(wham)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(here)
+source(file.path(here(),"code", "make_basic_info.R"))
+source(file.path(here(),"code", "set_NAA.R"))
+# create directory for analysis, e.g.
+
+
+write.dir <- file.path(here(),"NAA_re_study", "results")
+
+if(!exists("write.dir")) write.dir = getwd()
+if(!dir.exists(write.dir)) dir.create(write.dir, recursive = T)
+setwd(write.dir)
+
+#number of simulations for each scenario
+#nsim = 100
+nsim = 10
+
+#NAA sigmas for each scenario
+sigs = cbind(R_sig = c(0.3,0.5,0.7), NAA_sig = c(0.1,0.3,0.5))
+sigs_use = expand.grid(sig_ind = 1:NROW(sigs), ar1_y = c(NA,0.25,0.5), ar1_a = c(NA,0.25,0.5))
+
+df.mods = expand.grid(Recruitment = 2, R_sig = c(0.3,0.5,0.7), NAA_sig = c(NA), ar1_y = c(NA,0.25,0.5), ar1_a = NA)
+#df.mods = rbind(df.mods, cbind(Recruitment = 2, sigs[sigs_use$sig_ind,], sigs_use[,2:3]))
+df.mods = rbind(df.mods, expand.grid(Recruitment = 2, R_sig = c(0.3,0.5,0.7), NAA_sig = c(0.1,0.3,0.5), ar1_y = c(NA,0.25,0.5), ar1_a = c(NA,0.25,0.5)))
+
+n.mods <- dim(df.mods)[1] #90 scenarios!
+df.mods$Model <- paste0("m_",1:n.mods)
+df.mods <- df.mods %>% select(Model, everything()) # moves Model to first col
+
+# look at model table
+df.mods
+
+#make a list of input components that prepare_wham_input can use to generate an input for fit_wham
+groundfish_info = make_basic_info()
+
+selectivity = list(model = c(rep("logistic", groundfish_info$n_fleets),rep("logistic", groundfish_info$n_indices)),
+    initial_pars = rep(list(c(5,1)), groundfish_info$n_fleets + groundfish_info$n_indices)) #fleet, index
+
+M = list(initial_means = rep(0.2, length(groundfish_info$ages)))
+
+#initial numbers at age
+
+NAA_re = list(N1_pars = exp(10)*exp(-(0:(length(groundfish_info$ages)-1))*M$initial_means[1]))
+
+input = prepare_wham_input(basic_info = groundfish_info, selectivity = selectivity, M = M, NAA_re = NAA_re)
+
+
+sim_input = list()
+# sim the 90 models!
+for(m in 1:n.mods){
+
+    #initial numbers at age
+    NAA_re = list(N1_pars = exp(10)*exp(-(0:(length(groundfish_info$ages)-1))*M$initial_means[1]))
+    input = prepare_wham_input(basic_info = groundfish_info, selectivity = selectivity, M = M, NAA_re = NAA_re)
+    
+    NAA_re$use_steepness = 0
+    NAA_re$recruit_model = df.mods$Recruitment[m] #random effects with a constant mean
+    NAA_re$recruit_pars = exp(10) #mean recruitment, if recruit model changes, need to change this
+    
+    #whether RE on recruitment or on all NAA
+    if(is.na(df.mods$NAA_sig[m])) {
+      NAA_re$sigma = "rec" #random about mean
+      NAA_re$sigma_vals = df.mods$R_sig[m]
+    } else {
+      NAA_re$sigma = "rec+1"
+      NAA_re$sigma_vals = c(df.mods$R_sig[m], df.mods$NAA_sig[m])
+    }
+    
+    #what type of correlation structure
+    if(!is.na(df.mods$ar1_a[m]) & !is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "2dar1"
+        NAA_re$cor_vals = c(df.mods$ar1_a[m],df.mods$ar1_y[m])
+    }
+    if(is.na(df.mods$ar1_a[m]) & !is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "ar1_y"
+        NAA_re$cor_vals = df.mods$ar1_y[m]
+    }
+    if(!is.na(df.mods$ar1_a[m]) & is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "ar1_a"
+        NAA_re$cor_vals = df.mods$ar1_a[m]
+    }
+    if(is.na(df.mods$ar1_a[m]) & is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "iid"
+    }
+    input = set_NAA(input, NAA_re)
+
+    om = fit_wham(input, do.fit = FALSE)
+
+
+    #simulate data from operating model
+    set.seed(8675309) #use same seed for all operating models?
+    #all RE and data are simulated
+    sim_input[[m]] = lapply(1:nsim, function(x) {
+        input_i = input
+        sim = om$simulate(complete=TRUE)
+        input_i$data = sim
+        return(input_i)
+    })
+
+}
+
+#save simulated data sets to google drive?
+saveRDS(sim_input
+
+sim_fits = list()
+#for(m in 1:n.mods){
+m = 1
+    sim_fits[[m]] = lapply(1:nsim, function(x){
+        cat(paste("model:",m, "fit:", x, "start \n"))
+        out = fit_wham(sim_input[[m]][[x]], do.osa = FALSE, MakeADFun.silent = TRUE, retro.silent = TRUE, save.sdrep = FALSE)
+        cat(paste("model:",m, "fit:", x, "done \n"))
+        return(out)
+        })
+#}
+
+sapply(sim_fits[[1]], function(x) x$parList$log_NAA_sigma[1])
+
+###########################################################################
+#use more generic starting values
+
+make_basic_input <- function(years = 1982:2021, ages = 1:10) { #changed years
+    info = list()
+    info$ages = ages
+    info$years = years
+    na = length(info$ages)
+    ny = length(info$years)
+
+    info$n_fleets = 1
+    info$catch_cv = matrix(0.1, ny, info$n_fleets)
+    info$catch_Neff = matrix(200, ny, info$n_fleets)
+    info$n_indices = 1
+    info$index_cv = matrix(0.3, ny, info$n_indices)
+    info$index_Neff = matrix(100, ny, info$n_indices)
+    info$fracyr_indices = matrix(0.5, ny, info$n_indices)
+    info$index_units = rep(1, length(info$n_indices)) #biomass
+    info$index_paa_units = rep(2, length(info$n_indices)) #abundance
+    info$maturity = t(matrix(1/(1 + exp(-1*(1:na - na/2))), na, ny))
+
+    L = 100*(1-exp(-0.3*(1:na - 0)))
+    W = exp(-11)*L^3
+    nwaa = info$n_indices + info$n_fleets + 2
+    info$waa = array(NA, dim = c(nwaa, ny, na))
+    for(i in 1:nwaa) info$waa[i,,] = t(matrix(W, na, ny))
+
+    info$fracyr_SSB = rep(0.25,ny)
+    #info$q = rep(0.3, info$n_indices)
+    mid = floor(ny/2)
+    #up then down
+    info$F = matrix(0.2 + c(seq(0,0.4,length.out = ny/2),seq(0.4,0,length.out=ny-mid)),ny, info$n_fleets)
+
+    info$selblock_pointer_fleets = t(matrix(1:info$n_fleets, info$n_fleets, ny))
+    info$selblock_pointer_indices = t(matrix(info$n_fleets + 1:info$n_indices, info$n_indices, ny))
+    return(info)
+}
+basic_input = make_basic_input()
+
+selectivity = list(model = c(rep("logistic", groundfish_info$n_fleets),rep("logistic", groundfish_info$n_indices))) #fleet, index
+
+M = list(initial_means = rep(0.2, length(groundfish_info$ages)))
+
+#initial numbers at age
+NAA_re = list(N1_pars = rep(exp(10), length(groundfish_info$ages)))
+
+fit_init_par = list()
+# sim the 90 models!
+for(m in 1:n.mods){
+
+    if(is.na(df.mods$NAA_sig[m])) {
+        NAA_re$sigma = "rec" #random about mean
+    } else NAA_re$sigma = "rec+1"
+    
+
+    if(!is.na(df.mods$ar1_a[m]) & !is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "2dar1"
+    }
+    if(is.na(df.mods$ar1_a[m]) & !is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "ar1_y"
+    }
+    if(!is.na(df.mods$ar1_a[m]) & is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "ar1_a"
+    }
+    if(is.na(df.mods$ar1_a[m]) & is.na(df.mods$ar1_y[m])) {
+        NAA_re$cor = "iid"
+    }
+    
+    NAA_re$use_steepness = 0
+    NAA_re$recruit_model = df.mods$Recruitment[m] #random effects with a constant mean
+
+    fit_init_par[[m]] = prepare_wham_input(basic_info = basic_input, selectivity = selectivity, M = M, NAA_re = NAA_re)$par
+}
+
+sim_fits_2 = list()
+#for(m in 1:n.mods){
+m = 1
+    sim_fits_2[[m]] = lapply(1:nsim, function(x){
+        input = sim_input[[m]][[x]]
+        input$par = fit_init_par[[m]]
+        cat(paste("model:",m, "fit:", x, "start \n"))
+        out = fit_wham(input, do.osa = FALSE, MakeADFun.silent = TRUE, retro.silent = TRUE, save.sdrep = FALSE)
+        cat(paste("model:",m, "fit:", x, "done \n"))
+        return(out)
+        })
+#}
+cbind(sapply(sim_fits[[1]], function(x) x$parList$log_NAA_sigma[1]), 
+    sapply(sim_fits_2[[1]], function(x) x$parList$log_NAA_sigma[1]))
+
