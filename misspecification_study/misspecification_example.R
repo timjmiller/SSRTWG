5a86232ced45bf1cf04e905ca9b171282e75095b
cmlegault <chris.legault@noaa.gov>
2022-05-02 08:50:05 -0400

    clean up code and add SSB comparison

--- misspecification_study/misspecification_example.R
+++ misspecification_study/misspecification_example.R
@@ -17,7 +17,7 @@ if(!dir.exists(write.dir)) dir.create(write.dir, recursive = T)
 setwd(write.dir)
 
 # how many simulations
-nsim <- 1
+nsim <- 4
 
 # create input
 groundfish_info <- make_basic_info()
@@ -42,17 +42,8 @@ input <- prepare_wham_input(basic_info = groundfish_info, selectivity = gf_selec
 # run starter input
 om <- fit_wham(input, do.fit = FALSE, MakeADFun.silent = TRUE)
 
-#fit a SCAA model instead of Recruits as random effects? if so, comment out sigma, cor, recruit_model
-NAA_re_em <- list(
-  sigma = "rec", #random about mean
-  cor="iid", #random effects are independent
-  use_steepness = 0,
-  recruit_model = 2, #random effects with a constant mean
-  recruit_pars = exp(10),
-  N1_pars = exp(10)*exp(-(0:(length(groundfish_info$ages)-1))*M_om$initial_means[1])
-)
-em_input <- prepare_wham_input(basic_info = groundfish_info, NAA_re = NAA_re_em, selectivity = selectivity_om, M = M_om)
-agg_catch_multiplier <- create_agg_catch_multiplier(em_input)
+# set up estimation model
+em_input <- input # self-test
 
 #simulate data from operating model
 #all RE and data are simulated
@@ -69,7 +60,7 @@ sim_input[[1]] = lapply(1:nsim, function(x) {
 
 # sim_input[[2]] has catch data modification
 set.seed(8675309) #use same seed for all operating models?
-agg_catch_multiplier <- create_agg_catch_multiplier(input, multiplier=2)
+agg_catch_multiplier <- create_agg_catch_multiplier(input, multiplier=0.25)
 sim_input[[2]] = lapply(1:nsim, function(x) {
   input_i = em_input #maybe changed to not estimate recruits as RE
   sim = om$simulate(complete=TRUE)
@@ -104,10 +95,10 @@ ggplot(df, aes(x=Year, y=agg_catch, color=Source)) +
 ####################################
 #test fitting of one data set
 #look at ratio of osbvec agg_catches
-exp(sim_input[[1]][[1]]$data$obsvec[sim_input[[1]][[1]]$data$keep_C+1]-sim_input[[2]][[1]]$data$obsvec[sim_input[[2]][[1]]$data$keep_C+1])
-
-out = fit_wham(sim_input[[1]][[1]], do.osa = FALSE, do.retro = FALSE)
-out2 = fit_wham(sim_input[[2]][[1]], do.osa = FALSE, do.retro = FALSE)
+exp(sim_input[[1]][[1]]$data$obsvec[sim_input[[1]][[1]]$data$keep_C+1]-sim_input[[2]][[1]]$data$obsvec[sim_input[[2]][[1]]$data$keep_C+1])  
+#out = fit_wham(sim_input[[1]][[1]], do.osa = FALSE, do.retro = FALSE)
+#out2 = fit_wham(sim_input[[2]][[1]], do.osa = FALSE, do.retro = FALSE)
+#cbind(out$years, out$rep$SSB, out2$rep$SSB) # getting different results, so working
 ####################################
 
 # run the models
@@ -122,13 +113,27 @@ for(m in 1:2){
 }
 saveRDS(em_fits, file.path(write.dir, "em_fits.RDS"))
 
-# hmmm, something did not work, getting same results from original and modified catch
-mohns_rho(em_fits[[1]][[4]])
-mohns_rho(em_fits[[2]][[4]])
 
-mohns_rho(em_fits[[1]][[1]])
-mohns_rho(em_fits[[2]][[1]])
+# compare SSB time series
+df2 <- tibble(Source = character(),
+              Year = integer(),
+              Sim = integer(),
+              SSB = double())
 
-em_fits[[1]][[1]]$report()$SSB
-em_fits[[2]][[1]]$report()$SSB
-
+for(i in 1:nsim){
+  thisdf1 <- tibble(Source = "orig",
+                    Year = input$years,
+                    Sim = i,
+                    SSB = em_fits[[1]][[i]]$report()$SSB)
+  thisdf2 <- tibble(Source = "modified",
+                    Year = input$years,
+                    Sim = i,
+                    SSB = em_fits[[2]][[i]]$report()$SSB)
+  df2 <- rbind(df2, thisdf1, thisdf2)
+}
+ggplot(df2, aes(x=Year, y=SSB, color=Source)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~Sim, ncol=2) +
+  ylim(0, NA) +
+  theme_bw()
