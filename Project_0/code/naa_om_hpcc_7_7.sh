a1af88dbc825bc0ffd5d00bfd88e538d00ef6b6f
timjmiller <timothy.j.miller@noaa.gov>
2022-07-14 14:15:55 -0400

    fix hpcc code

--- Project_0/code/naa_om_hpcc_7_7.sh
+++ Project_0/code/naa_om_hpcc_7_7.sh
@@ -4,5 +4,17 @@ module load gcc/8.1.0
 module load R/4.0.4_gcc
 module load libjpeg-turbo/2.0.2
 
+echo "CPU threads: $(grep -c processor /proc/cpuinfo)"
+grep 'cpu cores' /proc/cpuinfo | uniq
+
 cd ~/SSRTWG
-Rscript --vanilla ~/SSRTWG/Project_0/code/naa_om_hpcc_script.R 7 7 480
+for sim in {7..7}
+do
+  for om in {1..24}
+  do
+    for em in {1..20}
+    do
+      Rscript --vanilla ~/SSRTWG/Project_0/code/naa_om_simf_fit_script_hpcc.R sim om em
+    done
+  done
+done
