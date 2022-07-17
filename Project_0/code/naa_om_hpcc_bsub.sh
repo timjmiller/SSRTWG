a61713ed793ff3c51fba25ea87e10d84a6339b4f
timjmiller <timothy.j.miller@noaa.gov>
2022-07-17 08:45:40 -0400

    increase bsub time request to max allowed

--- Project_0/code/naa_om_hpcc_bsub.sh
+++ Project_0/code/naa_om_hpcc_bsub.sh
@@ -10,7 +10,7 @@ do
   for ((this_om= $3; this_om <= $4; this_om++)) #om in {$omstart..$omend}
   do
     echo $this_om
-    bsub -n 20 -q short -W 2:00 -o ~/logs/logfile.%J -R "rusage[mem=5000]" -R "span[hosts=1]" -J naa_om_sim "bash ~/SSRTWG/Project_0/code/naa_om_hpcc_args.sh $this_sim $this_sim $this_om $this_om 1 20"
+    bsub -n 20 -q short -W 4:00 -o ~/logs/logfile.%J -R "rusage[mem=5000]" -R "span[hosts=1]" -J naa_om_sim "bash ~/SSRTWG/Project_0/code/naa_om_hpcc_args.sh $this_sim $this_sim $this_om $this_om 1 20"
   done
 done
 echo "script is done"
