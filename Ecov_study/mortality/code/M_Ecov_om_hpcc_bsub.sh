818a0e1649827b0603fa0b390dd72c713df34fc5
timjmiller <timothy.j.miller@noaa.gov>
2023-02-14 17:21:03 -0500

    fixing code

--- Ecov_study/mortality/code/M_Ecov_om_hpcc_bsub.sh
+++ Ecov_study/mortality/code/M_Ecov_om_hpcc_bsub.sh
@@ -8,12 +8,12 @@
 #$6 is last em
 #$7 is ncores per job (20 or less being the number of ems specified)
 
-for ((this_om= $3; this_om <= $4; this_om++)) #sim in {$simstart..$simend}
+for ((thisom= $3; thisom <= $4; thisom++)) #sim in {$simstart..$simend}
 do
-  for ((this_em= $5; this_em <= $6; this_em++)) #om in {$omstart..$omend}
+  for ((thisem= $5; thisem <= $6; thisem++)) #om in {$omstart..$omend}
   do
     echo $this_om
-    bsub -n $7 -q short -W 4:00 -o ~/logs/sims_$1_to_$2_om_$(this_om)_em_$(this_em).log -R "rusage[mem=5000]" -R "span[hosts=1]" -J M_ecov_om_sim "bash ~/SSRTWG/Ecov_study/mortality/code/M_Ecov_om_hpcc_args.sh $1 $2 $this_om $this_om $this_em $this_em"
+    bsub -n $7 -q short -W 4:00 -o ~/logs/sims_$1_to_$2_om_$thisom_em_$thisem.log -R "rusage[mem=5000]" -R "span[hosts=1]" -J M_ecov_om_sim "bash ~/SSRTWG/Ecov_study/mortality/code/M_Ecov_om_hpcc_args.sh $1 $2 $thisom $thisom $thisem $thisem"
   done
 done
 echo "script is done"
