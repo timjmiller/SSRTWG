d7678f962a77b4ccf41bc338d3708d2f3753d4f6
Amanda Hart <amanda.hart@noaa.gov>
2023-10-03 10:44:56 -0400

    Set up function to combine post-processed files.

--- Ecov_study/catchability/combinePerfMet.R
+++ Ecov_study/catchability/combinePerfMet.R
@@ -3,20 +3,40 @@
 #' Combine multiple files with post-processed performance metrics, the major action this function performs is renumbering sims so each sim has a unique number
 #' 
 #' @param filenames A vector of strings (including full file path and .RData extensions) for performance metrics files to 
+#' @param outdir A string for the directory where a plot folder will be generated
 
 
 
-combinePerfMet <- function(filenames=NULL){
+combinePerfMet <- function(filenames=NULL, outdir = here::here){
   
-  # Read in file
+  aggPerfMet <- NULL
   
-  # if first file do nothing
-  
-  # For subsequent files:
-    # Find unique sim
-    # generate unique sims starting 1 after largest sim in first file
-    # replace original unique sim with regenerated values that continue numbering from prior file
-  
-  # save results
+  for(ifile in 1:length(filenames)){
+    # Read in RDS file
+    perfMet <- readRDS(file = here::here(filenames[ifile]))
+    
+    
+    if(ifile == 1){ # If this is the first file, append to the aggPerfMet storage
+      aggPerfMet <- rbind(aggPerfMet, perfMet)
+    } else{
+      # ID number of sims to append
+      nsim <- perfMet$sim %>% unique() %>% length()
+      # ID last sim number in aggPerfMet 
+      lastsim <- aggPerfMet$sim %>% unique() %>% max()
+      # New sim numbers
+      newSim <- lastsim + 1:nsim
+      # Replace old sim numbers with new sim numbers
+      for(isim in 1:nsim){
+        perfMet[which(perfMet$sim == isim), "sim"] <- newSim[isim]
+      }
+      # Append newly renumbered simulations to aggPerfMet storage
+      aggPerfMet <- rbind(aggPerfMet, perfMet)
+      
+    }
+  }
+ 
+  # Save returned aggPerfMet in outdir
+  timeStamp <- Sys.time() %>% gsub(" ", "_",.) %>% gsub(":", "-", .) # Change millisecond half of Sys.time() output to avoid having spaces/weird characters in filenames
+  saveRDS(aggPerfMet, file = paste0(outdir, paste0("/aggPerformanceMetric_",timeStamp, ".Rds")))
   
 }
