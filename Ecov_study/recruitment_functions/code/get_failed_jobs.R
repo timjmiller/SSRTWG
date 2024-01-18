5c904dceccea4e9a918e037785678b7568bc5c91
Gregory Britten <gregleebritten@gmail.com>
2024-01-18 10:18:48 -0500

    failed jobs

--- Ecov_study/recruitment_functions/code/get_failed_jobs.R
+++ Ecov_study/recruitment_functions/code/get_failed_jobs.R
@@ -20,13 +20,13 @@ fail.list <- list()
 
 for (i in 1:n.om) {
   sims.run <- list.files(file.path(here::here(),"Ecov_study", "recruitment_functions", "results", om.names[i]), pattern = ".RDS")
-  sims.run <- list.files(paste0('~/dropbox/working/state_space_assessments/cluster_download/results/',om.names[i]))
+  #sims.run <- list.files(paste0('~/dropbox/working/state_space_assessments/cluster_download/results/',om.names[i]))
   fails <- which((sim.names %in% sims.run)==FALSE)
   
   if(length(fails)==0) fail.list[[i]] <- NA
   
   if(length(fails>0)) {
-    om.fails <- as.numeric(substr(om.names[i], 3, nchar(om.names[i]) ) )
+    om.fails <- as.numeric(substr(om.names[i], 3, nchar(_) ) )
     tmp <- strsplit(sim.names[fails], split="_")
     iter.fails <- as.numeric(c(unlist(lapply(tmp, function(x) {substr(x[1], 4, nchar(x[1]) ) }))) )
     em.fails <- as.numeric(c(unlist( lapply(tmp, function(x) {substr(x[2], 3, 3 ) }))  ) )
