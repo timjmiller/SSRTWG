f2c5c3249d5cc0123cb2d0cb164d952b82e2fb91
Gregory Britten <gregleebritten@gmail.com>
2024-07-23 10:59:28 -0400

    AIC analysis

--- Ecov_study/recruitment_functions/code/analysis/AIC_dataframe.R
+++ Ecov_study/recruitment_functions/code/analysis/AIC_dataframe.R
@@ -6,7 +6,9 @@ library(wham)
 source(file.path(here::here(), "Ecov_study","recruitment_functions", "code", "analysis", "functions_for_analysis.R" ) )
 
 #res.path <- 'E:/results_beta_fix'  # directory where simulation runs are (beta unstandardized)
-res.path <- file.path(here::here(),"Ecov_study","recruitment_functions","results")  # directory where simulation runs are (beta unstandardized)
+res.path    <- file.path(here::here(),"Ecov_study","recruitment_functions","results")  # directory where simulation runs are (beta unstandardized)
+save.path   <- file.path(here::here(),"Ecov_study",'recruitment_functions', 'results')  
+save.suffix <- ''
 
 df.oms    <- readRDS(file.path(here::here(),"Ecov_study","recruitment_functions", "inputs", "df.oms.RDS"))
 df.ems    <- readRDS(file.path(here::here(),"Ecov_study", "recruitment_functions", "inputs", "df.ems.RDS"))
@@ -21,11 +23,10 @@ n_sims <- 100
 ## AIC_weight has info on all EMs per OM-Sim
 ## both dfs contain convergence info
 ################################################################################
-t4 <- Sys.time()
-get_aic_convergence_info(df.oms, df.ems, nsims=n_sims, res.path=res.path, save.rds=TRUE, save.suffix='_beta_fix', save.path=here::here("Ecov_study", 'recruitment_functions', 'results_beta_fix')  ) 
-# "This took 20.7794454693794" minutes (liz's runs)!  
-cd
+get_aic_convergence_info(df.oms, df.ems, 
+                         nsims=n_sims, 
+                         res.path=res.path, 
+                         save.rds=TRUE, 
+                         save.suffix=save.suffix, 
+                         save.path=save.path) 
 
-
-t5 <- Sys.time()
-t5-t4   # Time difference of 4.075107 hours
